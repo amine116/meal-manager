eb37eb02a2cd021448a7843b43871f37b91ee0f9
amine116 <iaminul237@gmail.com>
2021-08-15 13:39:17 +0600

    Version 11.30.7

--- app/src/main/java/com/amine/mealmanager/ActivityInstructions.java
+++ app/src/main/java/com/amine/mealmanager/ActivityInstructions.java
@@ -30,6 +30,7 @@ public class ActivityInstructions extends AppCompatActivity implements View.OnCl
                 txtGitlabLink = findViewById(R.id.txtGitlabLink);
         t2.setMovementMethod(LinkMovementMethod.getInstance());
         txtGitlabLink.setMovementMethod(LinkMovementMethod.getInstance());
+        //Just for nothing bro :3 :v
     }
 
     @Override
